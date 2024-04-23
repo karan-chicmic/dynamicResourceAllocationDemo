023f92f7a1e8ad45dd6a8859cc796d1162ec9239
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-23 17:12:10 +0530

    dynamically prefab loading fine

--- assets/script/customScript.ts
+++ assets/script/customScript.ts
@@ -1,20 +1,29 @@
-import { _decorator, Component, Node, resources, Sprite, SpriteFrame } from "cc";
+import { _decorator, Component, instantiate, Node, Prefab, resources, Sprite, SpriteFrame } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("customScript")
 export class customScript extends Component {
-    @property({ type: Sprite })
-    person: Sprite = null;
-    start() {}
+    // @property({ type: Sprite })
+    // person: Sprite = null;
+    start() {
+        for (let i = 0; i < 5; i++) {
+            resources.load("prefabs/nodePrefab", Prefab, null, (err, prefab) => {
+                console.log(typeof prefab);
+                console.log(prefab);
+                const newNode = instantiate(prefab);
+                this.node.addChild(newNode);
+            });
+        }
+    }
 
     update(deltaTime: number) {}
 
-    onClick() {
-        resources.load("person/person1/spriteFrame", SpriteFrame, null, (err, spriteFrame) => {
-            console.log("frame: ", spriteFrame);
-            console.log(err);
-            // this.node.getComponent(Sprite).spriteFrame = spriteFrame;
-            this.person.spriteFrame = spriteFrame;
-        });
-    }
+    // onClick() {
+    //     resources.load("person/person1/spriteFrame", SpriteFrame, null, (err, spriteFrame) => {
+    //         console.log("frame: ", spriteFrame);
+    //         console.log(err);
+    //         // this.node.getComponent(Sprite).spriteFrame = spriteFrame;
+    //         this.person.spriteFrame = spriteFrame;
+    //     });
+    // }
 }
